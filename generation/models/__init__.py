f51416709abe6cef46562217f00e381d5987a31f
Quinut-McGee <124704415+Quinut-McGee@users.noreply.github.com>
2025-10-19 13:42:54 -0500

    “diffintro”

--- generation/models/__init__.py
+++ generation/models/__init__.py
@@ -2,7 +2,9 @@
 SOTA model implementations for competitive mining
 """
 
-from .flux_generator import FluxImageGenerator
+# Only import what we're using to avoid dependency issues
+# from .flux_generator import FluxImageGenerator
+from .sdxl_turbo_generator import SDXLTurboGenerator
 from .background_remover import SOTABackgroundRemover
 
-__all__ = ["FluxImageGenerator", "SOTABackgroundRemover"]
+__all__ = ["SDXLTurboGenerator", "SOTABackgroundRemover"]
